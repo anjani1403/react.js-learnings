3e0c482884b4d41a99765ad5d666e519f28a6cb6
Anjani Manthapurwar <anjanimanthapurwar1403@gmail.com>
2026-01-03 08:49:06 +0530

    Practiced React props with Card component

---
+++ 03tailwindprops/vite.config.js
@@ -0,0 +1,11 @@
+import tailwind from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwind(),
+  ],
+})
